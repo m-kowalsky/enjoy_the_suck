58ed5aaf289da51ff634dc04a5ac1047e7891a4a
carlhavoc <mkowalsky87@gmail.com>
2025-06-30 17:24:38 -0700

    initial server up and running

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.LoadHTMLGlob("views/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Enjoy Sucking"})
+	})
+	router.Run(":8080")
+}
