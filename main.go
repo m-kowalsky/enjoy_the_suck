764b69acf7953382cacc76918ad03bffecd8ee0b
carlhavoc <mkowalsky87@gmail.com>
2025-07-01 16:44:15 -0700

    kamal

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"title": "Enjoy Sucking"})
 	})
-	router.Run(":8080")
 
 	router.GET("/kamal/up/", func(c *gin.Context) {
 		c.Status(200)
 	})
+	router.Run(":8080")
 }
